8affd13cbaf5e7363b7f492041ba90a6a7277169
Reid Harry <reidoharry50@gmail.com>
2020-01-26 15:58:08 -0500

    added week 02 lecture examples

---
+++ Homeworks/04.3.cpp
@@ -0,0 +1,20 @@
+/*  Reid Harry
+    1/26/2020
+    Write a program that takes in two integers. It multiples the first number by the second, and prints the product on its own line. 
+    Then multiples this product by the second number, repeating while the number is less than 10,000.
+*/
+
+#include<iostream>
+using std::cin; using std::cout; using std::endl;
+
+int main(){
+    // take two ints at input
+    int num1, num2;
+    cin >> num1 >> num2;
+
+    int product = num1 * num2;
+    do{
+        cout << product << endl;
+        product *= num2;
+    }while(product < 10000);
+}
