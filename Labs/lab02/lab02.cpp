52c624f16d97f6b518964168110071a880865cb7
Reid Harry <reidoharry50@gmail.com>
2020-01-30 18:30:16 -0500

    done with lab02!

--- Labs/lab02/lab02.cpp
+++ Labs/lab02/lab02.cpp
@@ -2,5 +2,29 @@
 using std::cin; using std::cout; using std::endl;
 
 int main(){
-    
+    int count = 0;
+    int sum = 0;
+    int input;
+
+    cin >> input;
+
+    if(input < 0){
+        cout << "Error";
+    }else{
+        while(input >= 10){
+            while(true){
+                if(input == 0){
+                    break;
+                }else{
+                    sum += (input % 10);
+                    input /= 10;
+                }
+            }
+            input = sum;
+            sum = 0;
+            count++;
+        }
+
+        cout << count << " " << input;
+    }
 }
