bbe331994847064752b9d7ab122ed886da2f9e3b
Reid Harry <reidoharry50@gmail.com>
2020-01-30 18:41:31 -0500

    added comments to lab02

--- Labs/lab02/lab02.cpp
+++ Labs/lab02/lab02.cpp
@@ -1,28 +1,31 @@
 #include<iostream>
-using std::cin; using std::cout; using std::endl;
+using std::cin; using std::cout;
 
 int main(){
-    int count = 0;
-    int sum = 0;
-    int input;
+    int count = 0;  // additive persistance
+    int sum = 0;    // used in summing up the digits
+    int input;      // used to store user input and additive root
 
+    // get user's interger input
     cin >> input;
 
-    if(input < 0){
+    // finds additive persistance and additive root
+    if(input < 0){  // negatives cannot be taken additive root of
         cout << "Error";
     }else{
+        // continue while the number is in the double digits
         while(input >= 10){
             while(true){
-                if(input == 0){
+                if(input == 0){     // if all the digits have been used
                     break;
                 }else{
-                    sum += (input % 10);
-                    input /= 10;
+                    sum += (input % 10);    // add one of the digits
+                    input /= 10;    // move to the next digits
                 }
             }
-            input = sum;
-            sum = 0;
-            count++;
+            input = sum;    // put sum into input to loop through every digit
+            sum = 0;    // reset sum
+            count++;    // add to additive persistance
         }
 
         cout << count << " " << input;
