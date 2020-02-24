be927d09f3eef6e1f5db25e65a9c89d649a77e25
Reid Harry <reidoharry50@gmail.com>
2020-02-24 17:11:39 -0500

    finished 11.2

--- Homeworks/11.2.cpp
+++ Homeworks/11.2.cpp
@@ -4,4 +4,14 @@
     arguments. However, if the second argument (the demoninator) is zero, raise a std::runtime_error.
 */
 
-template<typename
+#include<stdexcept>
+using std::runtime_error;
+
+template<typename T>
+T safe_divide(T dividend, T divisor){
+    if(divisor == 0){
+        throw runtime_error("division by zero");
+    }
+
+    return dividend/divisor;
+}
