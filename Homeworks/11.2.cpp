51d963a06ced65d4a1802f5db011381002d1d11b
Reid Harry <reidoharry50@gmail.com>
2020-02-24 16:38:54 -0500

    started 11.2

---
+++ Homeworks/11.2.cpp
@@ -0,0 +1,7 @@
+/*  Reid Harry
+    2/24/2019
+    Write a templated function named safe_divide that returns the result of division of its two 
+    arguments. However, if the second argument (the demoninator) is zero, raise a std::runtime_error.
+*/
+
+template<typename
