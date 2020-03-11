02c8379267dfc0497a779ea284d4428811c0deff
Reid Harry <reidoharry50@gmail.com>
2020-03-11 17:16:59 -0400

    started 13.4.cpp

--- Homeworks/13.2.cpp
+++ Homeworks/13.2.cpp
@@ -5,6 +5,7 @@
     reorder the vector so that the courses are sorted by number in ascending order. You can assume 
     that the department code is separated from the number by a space and that all course numbers 
     are 3 characters long.
+    No looping!!!
 */
 
 #include<string>
---
+++ Homeworks/13.4.cpp
@@ -0,0 +1,16 @@
+/*  Reid Harry
+    3/11/2020
+    Write a function, named seriesOfLetters, that takes a reference to a vector of chars and returns nothing. 
+    The function should modify the vector to contain a series of letters increasing from the smallest letter 
+    (by ASCII value) in the vector. Note, the vector should not change in size, and the initial contents of the 
+    vector (excepting the smallest letter) do not matter.
+*/
+
+#include<vector>
+using std::vector;
+#include<algorithm>
+using std::sort;
+
+void seriesOfLetters(vector<char> chars){
+    sort(chars.begin(), chars.end());
+}
